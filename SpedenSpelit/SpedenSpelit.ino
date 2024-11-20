a59c1d07db69bfe93863b79b9bc4c2176056c3e0
veliok <22372308+veliok@users.noreply.github.com>
2024-11-21 01:51:37 +0200

    Update SpedenSpelit.ino
    Added a test version

--- SpedenSpelit/SpedenSpelit.ino
+++ SpedenSpelit/SpedenSpelit.ino
@@ -4,14 +4,13 @@
 #include "SpedenSpelit.h"
 
 
-volatile int buttonNumber = -1;            // for buttons interrupt handler
+volatile int buttonNumber = -1;           // for buttons interrupt handler
 volatile int pressCounter = 0;             // counter for button presses
-volatile bool newTimerInterrupt = false;   // for timer interrupt handler
-volatile int interruptCounter = 0;         // counter for interrupts
-int randomNumbers[100];                    // stores random integers
+int randomNumbers[100];          // stores random integers
 volatile int userNumbers[100];             // stores button presses
-volatile int score = 0;
-volatile bool running = false;
+volatile int gameScore = 0;
+int gameMode = 0;
+volatile bool isRunning = false;
 
 
 void setup()
@@ -23,30 +22,29 @@ void setup()
 
 void loop()
 {
-  if(buttonNumber>=0)
+  if(!isRunning)
   {
-    if(buttonNumber == 4)
+    if(buttonNumber >= 0 && buttonNumber < 4)
     {
-      startTheGame();  // start the game if buttonNumber == 4
+      gameMode = buttonNumber;
+      isRunning = true;
+      buttonNumber = -1;
     }
-    else if(running && buttonNumber < 4)
-    {
-      pressCounter++;
-      checkGame(buttonNumber);  // check the game if 0<=buttonNumber<4
-    }
-    buttonNumber = -1;  // reset buttonNumber after handling
   }
 
-  if(running && newTimerInterrupt)
+  if(isRunning)
   {
-    setLed(randomNumbers[interruptCounter]);    // interrupt turns new led on
-    newTimerInterrupt = false;
+    if(buttonNumber >= 0 && buttonNumber < 4)
+    {
+      checkGame();
+      buttonNumber = -1;
+      pressCounter++;
+    }
   }
 }
 
 void initializeTimer(void)
 {
-  TCNT1 = 0;
   OCR1A = 15624;                          // Generates 1hz timer cycle (16MHz/1024-1 = 15624)
   TCCR1B |= (1 << WGM12);                 // CTC-mode enabled
   TCCR1B |= (1 << CS12) | (1 << CS10);    // prescaler 1024
@@ -55,49 +53,39 @@ void initializeTimer(void)
 
 ISR(TIMER1_COMPA_vect)
 {
-  interruptCounter++;
-  if(interruptCounter % 10 == 0) 
-  {
-    OCR1A -= 1562;   // Increase timer interrupt rate after 10 interrupts MODIFY IF TOO FAST/SLOW
-    if(OCR1A < 1000)
-    {
-      OCR1A = 1000;  // lower limit for rate
-    }
-  }
-  newTimerInterrupt = true;
+  OCR1A -= 1562;   // Increase timer interrupt rate after 10 interrupts MODIFY IF TOO FAST/SLOW
+  if(OCR1A < 1000) OCR1A = 1000; // lower limit for rate
 }
 
 void checkGame(byte nbrOfButtonPush)
 {
-  userNumbers[pressCounter] = nbrOfButtonPush;
   if(randomNumbers[pressCounter] != userNumbers[pressCounter]) // stop game if button press is wrong and display score
   {
-    running = false;
+    isRunning = false;
+    Serial.println("Game ended");
   }
-  else  // game continues
-  {
-    score++;
-  }
-  showResult(score);  // display score
+  else gameScore++;
+  //showResult(gameScore);  // display score
 }
 
 void initializeGame()
 {
-  score = 0;
+  gameScore = 0;
   pressCounter = 0;
-  interruptCounter = 0;
-  running = true;
+  isRunning = true;
+  buttonNumber = -1;
 
   for(int i = 0; i < 100; i++)    // fill array with 100 random numbers from 0-3
   {
     randomNumbers[i] = random(0, 4);
   }
-  initializeTimer(); // initialize here, so timer resets on round start
+  Serial.println("Game initialized");
 }
 
 void startTheGame()
 {
   initializeGame();
-  showResult(score);
+  initializeTimer();
+  showResult(gameScore);
 }
 

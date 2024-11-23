7bf08e13a86055d5e48a4629b5c3b923e99fa72b
veliok <22372308+veliok@users.noreply.github.com>
2024-11-23 18:58:38 +0200

    Update SpedenSpelit.ino
    Moved game functionality outside timer interrupt handler

--- SpedenSpelit/SpedenSpelit.ino
+++ SpedenSpelit/SpedenSpelit.ino
@@ -4,13 +4,15 @@
 #include "SpedenSpelit.h"
 
 
-volatile int buttonNumber = -1;           // for buttons interrupt handler
-volatile int pressCounter = 0;             // counter for button presses
-int randomNumbers[100];          // stores random integers
+volatile int buttonNumber = -1;            // for buttons interrupt handler
+volatile int pressCounter = 0;
+volatile int interruptCounter = -1;
+int randomNumbers[100];                    // stores random integers
 volatile int userNumbers[100];             // stores button presses
 volatile int gameScore = 0;
 int gameMode = 0;
 volatile bool isRunning = false;
+volatile bool timerInterrupt = false;
 
 
 void setup()
@@ -26,9 +28,7 @@ void loop()
   {
     if(buttonNumber >= 0 && buttonNumber < 4)
     {
-      gameMode = buttonNumber;
-      isRunning = true;
-      buttonNumber = -1;
+      startTheGame();
     }
   }
 
@@ -36,16 +36,54 @@ void loop()
   {
     if(buttonNumber >= 0 && buttonNumber < 4)
     {
-      checkGame();
-      buttonNumber = -1;
-      pressCounter++;
+      checkGame(buttonNumber);
+      Serial.print("LED in array: ");
+      Serial.print(randomNumbers[pressCounter]);
+      Serial.print(" - NUM in array: ");
+      Serial.println(userNumbers[pressCounter]);
+    }
+    
+    if(timerInterrupt) // Timer interrupt
+    {
+      interruptCounter++;
+      int ledToWrite = randomNumbers[interruptCounter];
+
+      if(isRunning && interruptCounter != 0) // Only set leds if game is running and gameMode has been selected
+      {
+        if(gameMode == 0) // Normal game mode
+        {
+          setLed(ledToWrite);
+        }
+
+        if(gameMode == 1) // Faster game mode
+        {
+          setLed(ledToWrite);
+          OCR1A = OCR1A - 1248;
+          if(OCR1A < 3592) OCR1A = 3592;
+        }
+
+        if(gameMode == 2) // Inverted led game mode
+        {
+          for(int i = 0; i < 4; i ++)
+          {
+            if(i != ledToWrite) setLed(i);
+          }
+        }
+
+        OCR1A = OCR1A - 624;   // Increase timer interrupt rate by ~40ms
+        if(OCR1A < 3592) OCR1A = 3592; // lower limit for rate
+      }
+      timerInterrupt = false;
     }
   }
 }
 
 void initializeTimer(void)
 {
-  OCR1A = 15624;                          // Generates 1hz timer cycle (16MHz/1024-1 = 15624)
+  TCCR1A = 0;               
+  TCCR1B = 0;               
+  TCNT1 = 0;
+  OCR1A = 46875;                          // Generates 3s timer cycle
   TCCR1B |= (1 << WGM12);                 // CTC-mode enabled
   TCCR1B |= (1 << CS12) | (1 << CS10);    // prescaler 1024
   TIMSK1 |= (1 << OCIE1A);                // enable timer compare
@@ -53,27 +91,34 @@ void initializeTimer(void)
 
 ISR(TIMER1_COMPA_vect)
 {
-  OCR1A -= 1562;   // Increase timer interrupt rate after 10 interrupts MODIFY IF TOO FAST/SLOW
-  if(OCR1A < 1000) OCR1A = 1000; // lower limit for rate
+  timerInterrupt = true;
 }
 
 void checkGame(byte nbrOfButtonPush)
 {
+  pressCounter++;
+  userNumbers[pressCounter] = nbrOfButtonPush;
+
   if(randomNumbers[pressCounter] != userNumbers[pressCounter]) // stop game if button press is wrong and display score
   {
     isRunning = false;
-    Serial.println("Game ended");
+    stopTheGame();
   }
-  else gameScore++;
-  //showResult(gameScore);  // display score
+
+  if(interruptCounter == 100) stopTheGame(); // Stop game after 100 rounds
+  gameScore++;
+  showResult(gameScore);
+  buttonNumber = -1;
 }
 
 void initializeGame()
 {
+  gameMode = buttonNumber;
   gameScore = 0;
   pressCounter = 0;
   isRunning = true;
   buttonNumber = -1;
+  interruptCounter = 0;
 
   for(int i = 0; i < 100; i++)    // fill array with 100 random numbers from 0-3
   {
@@ -87,5 +132,13 @@ void startTheGame()
   initializeGame();
   initializeTimer();
   showResult(gameScore);
+}
+
+void stopTheGame()
+{
+  Serial.println("Game ended");
+  Serial.print("End Score: ");
+  showResult(gameScore);
+  TIMSK1 &= ~(1 << OCIE1A); // Disable timer
 }
 
